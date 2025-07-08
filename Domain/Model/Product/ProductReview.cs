6c37bbb2f1e0c2cc50ad498157a6ecf357a5a05f
Volodymyr Nedividov <vladimir.nedividov@gmail.com>
2025-07-08 13:34:34 +0200

    fix

---
+++ Domain/Model/Product/ProductReview.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Product
+{
+    public class ProductReview
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Advantages { get; set; }
+        public string Disadvantages { get; set; }
+        public DateOnly CreatedAt { get; set; }
+        public int Rating { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+    }
+}
---
+++ Domain/Model/Product/ProductVariation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Product
+{
+    public class ProductVariation
+    {
+        public int Id { get; set; }
+
+        public int GroupId { get; set; }
+
+        public string Name { get; set; }
+        public List<string> Variations { get; set; }
+    }
+}
