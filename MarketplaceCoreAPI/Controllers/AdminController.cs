9a9b8ede1203e647cd91c25aa993fdc4731fe331
Volodymyr Nedividov <vladimir.nedividov@gmail.com>
2025-07-20 14:06:07 +0200

    fix

--- MarketplaceCoreAPI/Controllers/AdminController.cs
+++ MarketplaceCoreAPI/Controllers/AdminController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MarketplaceCoreAPI.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class AdminController : Controller
 {
     private readonly IAdminService  _service;
