019d4071c56d3900cbac98691337a03845616daa
Volodymyr Nedividov <vladimir.nedividov@gmail.com>
2025-10-28 13:34:01 +0100

    Make Swagger visible

--- MarketplaceCoreAPI/Program.cs
+++ MarketplaceCoreAPI/Program.cs
@@ -50,11 +50,9 @@ internal class Program
 
 
 // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+        
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
         
 
         app.UseCors("AllowAll");
